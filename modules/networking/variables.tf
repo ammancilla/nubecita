359d4b253f446dd21005efca4a33e969f51b4d3d
Alfonso Manuel <almancill@gmail.com>
2020-03-02 02:58:03 +0100

    Remove redundant type entry from variables declaration

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,23 +1,23 @@
 variable "environment" {
-  type = string
   description = "The working environment"
 }
 
 variable "region" {
-  type = string
   description = "AWS region"
 }
 
 variable "aws_vpc_cidr_block" {
-  type = string
   description = "VPC's cidr block"
 }
 
-variable "aws_vpc_name" {
-  type = string
-}
+variable "aws_vpc_name" {}
 
 variable "aws_vpc_location" {
-  type = string
+  description = "Location of the .tf file that defines the resource"
+}
+
+variable "aws_route_table_name" {}
+
+variable "aws_route_table_location" {
   description = "Location of the .tf file that defines the resource"
 }
