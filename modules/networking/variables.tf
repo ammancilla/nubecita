7dd02dd6bf5c20702873c817f5fbbd1151366676
Alfonso Manuel <almancill@gmail.com>
2020-03-03 23:51:04 +0100

    Add zero+ private subnets

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -6,6 +6,11 @@ variable "region" {
   description = "AWS region"
 }
 
+variable "availability_zones" {
+  type = list
+  description = "List of AZ of the AWS region"
+}
+
 variable "aws_vpc_cidr_block" {
   description = "VPC's cidr block"
 }
@@ -15,3 +20,7 @@ variable "aws_vpc_name" {}
 variable "aws_vpc_location" {
   description = "Location of the .tf file that defines the vpc"
 }
+
+variable "aws_subnet__private__name" {}
+variable "aws_subnet__private__count" {}
+variable "aws_subnet__private__location" {}
