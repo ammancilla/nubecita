47be440cc6eec4d40a565dee62887a8e1439c799
Alfonso Manuel <almancill@gmail.com>
2020-03-04 23:22:25 +0100

    Changes:
    - Add public subnets (one per AZ)
    - Remove hardcoded values for subnetcidr calculation
    - change VPC cidr from 10.0.0.0/16 to 10.10.0.0/16
    - remove custom count to create subnets. Instead, create one per AZ.

--- components/networking/variables.tf
+++ components/networking/variables.tf
@@ -7,6 +7,15 @@ variable "aws_vpc__cidr_block" {}
 variable "aws_vpc__name" {}
 variable "aws_vpc__location" {}
 
+variable "aws_subnet__cidrsubnet_newbits" {
+  default = 8
+}
+variable "aws_subnet__cidrsubnet__netnum" {
+  default = 1
+}
+
 variable "aws_subnet__private__name" {}
-variable "aws_subnet__private__count" {}
 variable "aws_subnet__private__location" {}
+
+variable "aws_subnet__public__name" {}
+variable "aws_subnet__public__location" {}
--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -52,16 +52,41 @@ resource "aws_vpc" "nubecita" {
 # main route table automatically
 #
 resource "aws_subnet" "private" {
-  count = var.aws_subnet__private__count
+  count = length(var.availability_zones)
 
   vpc_id = aws_vpc.nubecita.id
   availability_zone = var.availability_zones[count.index]
-  cidr_block = cidrsubnet(aws_vpc.nubecita.cidr_block, 8, count.index + 1)
+  cidr_block = cidrsubnet(
+    aws_vpc.nubecita.cidr_block,
+    var.aws_subnet__cidrsubnet_newbits,
+    var.aws_subnet__cidrsubnet__netnum + count.index
+  )
 
   tags = {
     Count = count.index
     Environment = var.environment
     Location = var.aws_subnet__private__location
     Name = "${var.aws_subnet__private__name}.${count.index}"
+    Tier = "private"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.availability_zones)
+
+  vpc_id = aws_vpc.nubecita.id
+  availability_zone = var.availability_zones[count.index]
+  cidr_block = cidrsubnet(
+    aws_vpc.nubecita.cidr_block,
+    var.aws_subnet__cidrsubnet_newbits,
+    length(var.availability_zones) + count.index + 1
+  )
+
+  tags = {
+    Count = count.index
+    Environment = var.environment
+    Location = var.aws_subnet__public__location
+    Name = "${var.aws_subnet__public__name}.${count.index}"
+    Tier = "public"
   }
 }
